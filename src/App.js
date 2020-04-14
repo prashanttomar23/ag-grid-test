b8c71bb4af24e1c8f908da1fbb19dbcb867a1cba
prashanttomar23 <prashant.tomar@rapidops.com>
2020-04-14 13:26:42 +0530

    Updated task 5

--- src/App.js
+++ src/App.js
@@ -22,14 +22,7 @@ export class App extends Component {
       }, {
         headerName: "Image", field: "profile_image",sortable:true
       }],
-      rowData:null
-      //       rowData: [{
-      //   make: "Toyota", model: "Celica", price: 35000
-      // }, {
-      //   make: "Ford", model: "Mondeo", price: 32000
-      // }, {
-      //   make: "Porsche", model: "Boxter", price: 72000
-      // }]  
+      rowData:null 
     }
   }
   componentDidMount(){
